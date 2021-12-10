75598497866fce62303077b6edfad85a2cc6a431
dev윾수 <kimdia2000@gmail.com>
2021-12-10 17:18:33 +0900

    스프링부트 기본

---
+++ src/main/java/hello/core/member/MemberApp.java
@@ -0,0 +1,13 @@
+package hello.core.member;
+
+public class MemberApp {
+    public static void main(String[] args) {
+        MemberService memberService = new MemberServiceImpl();
+        Member memberA = new Member(1L, "memberA", Grade.VIP);
+        memberService.join(memberA);
+
+        Member findMember = memberService.findMember(1L);
+        System.out.println("new member = " + memberA.getName());
+        System.out.println("find member = " + findMember.getName());
+    }
+}
