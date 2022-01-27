530d04e441be67bc6c623fcd32834f1af458c912
dev윾수 <kimdia2000@gmail.com>
2022-01-27 16:06:51 +0900

    220127

--- src/test/java/hello/core/beanFind/ApplicationContextInfoTest_1.java
+++ src/test/java/hello/core/beanFind/ApplicationContextInfoTest_1.java
@@ -12,9 +12,11 @@ public class ApplicationContextInfoTest_1 {
     @Test
     @DisplayName("모든 빈 출력하기")
     void findAllBean(){
+        // Bean에 선언한 이름들을 가져옴
         String[] beanDefinitionNames = ac.getBeanDefinitionNames();
         //iter 자동완성하면 바로 위의 배열이 포이치문으로 됨
         for (String beanDefinitionName : beanDefinitionNames) {
+            //이름으로 객체를 하나 가져온다
             Object bean = ac.getBean(beanDefinitionName);
             System.out.println("name = "+beanDefinitionName+" object = "+bean);
         }
@@ -22,6 +24,7 @@ public class ApplicationContextInfoTest_1 {
     @Test
     @DisplayName("애플리케이션 빈 출력하기")
     void findApplicationBean(){
+        // Bean에 선언한 이름들을 가져옴
         String[] beanDefinitionNames = ac.getBeanDefinitionNames();
         //iter 자동완성하면 바로 위의 배열이 포이치문으로 됨
         for (String beanDefinitionName : beanDefinitionNames) {
@@ -29,9 +32,11 @@ public class ApplicationContextInfoTest_1 {
 
             //Role ROLE_APPLICATION: 직접 등록한 애플리케이션 빈
             //Role ROLE_INFRASTRUCTURE: 스프링이 내부에서 사용하는 빈
+            Object bean = ac.getBean(beanDefinitionName);
             if(beanDefinition.getRole() == BeanDefinition.ROLE_APPLICATION){
-                Object bean = ac.getBean(beanDefinitionName);
-                System.out.println("name = "+beanDefinitionName+" object = "+bean);
+                System.out.println("Application Bean name = "+beanDefinitionName+", object = "+bean);
+            }else if(beanDefinition.getRole() == BeanDefinition.ROLE_INFRASTRUCTURE){
+                System.out.println("Infrastructure Bean name = "+beanDefinitionName+", object = "+bean);
             }
         }
     }
